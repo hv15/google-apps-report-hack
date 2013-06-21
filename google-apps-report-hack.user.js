d71e80edea3a6418510fd30f7a4132f6896910e6
Hans-Nikolai Viessmann <hansthespass@googlemail.com>
2013-06-21 14:31:33 +0100

    Google changed their selectors, updated. I have also added an include statement as well

--- google-apps-report-hack.user.js
+++ google-apps-report-hack.user.js
@@ -3,11 +3,13 @@
 // @namespace	https://github.com/hv15/google-apps-report-hack
 // @description	This solves (hackishly) the Google Apps problem relating to the reports not being fully shown by alterting some CSS.
 // @match	https://www.google.com/a/cpanel/*/AdminHome#Reports/subtab=emaillogs
+// @include	/^https://www.google.com/a/cpanel/.*/AdminHome#Reports/subtab=emaillogs$/
 // @grant	none
-// @version	1.0
+// @version	1.1
 // ==/UserScript==
 
-var div = document.getElementsByClassName('GGLYHMPCIBC');
+var selector = 'GLGLAIEIBC';
+var div = document.getElementsByClassName(selector);
 
 if(div.length == 1){
 	div.item(0).style.overflow = 'scroll';
